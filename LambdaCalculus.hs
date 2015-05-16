85039d7c61f4977cad3d7c4b589b68de1bff735b
volhovM <volhovm.cs@gmail.com>
2015-05-17 00:57:51 +0300

    added de-bruijn notation

--- LambdaCalculus.hs
+++ LambdaCalculus.hs
@@ -2,7 +2,8 @@
 
 module LambdaCalculus where
 
-import Data.Set (Set, empty, union, insert, singleton, member, toList)
+import qualified Data.Set as S
+import qualified Data.Map as M
 
 type Literal = String
 data Lambda = App Lambda Lambda | Abs Literal Lambda | Var Literal
@@ -18,13 +19,13 @@ outputView (Abs s l) = brck $ '\\' : (s ++ "." ++ outputView l)
 brck s = "(" ++ s ++ ")"
 
 
-freeVars' :: Lambda → Set Literal → Set Literal
-freeVars' (App a b) ignored = (freeVars' a ignored) `union` (freeVars' b ignored)
-freeVars' (Abs s l) ignored = freeVars' l $ insert s ignored
-freeVars' (Var s) ignored   = if member s ignored then empty else singleton s
+freeVars' :: Lambda → S.Set Literal → S.Set Literal
+freeVars' (App a b) ignored = (freeVars' a ignored) `S.union` (freeVars' b ignored)
+freeVars' (Abs s l) ignored = freeVars' l $ S.insert s ignored
+freeVars' (Var s) ignored   = if S.member s ignored then S.empty else S.singleton s
 
-freeVars :: Lambda → Set Literal
-freeVars l = freeVars' l empty
+freeVars :: Lambda → S.Set Literal
+freeVars l = freeVars' l S.empty
 
 -- into, instead of what, what
 substitute :: Lambda → Literal → Lambda → Either Literal Lambda
@@ -33,11 +34,32 @@ substitute (App l r) x a = case ((substitute l x a), (substitute r x a)) of
   (Right t, Right s) → Right $ App t s
   (Left t, Right _)  → Left t
   (_, Left t)        → Left t
-substitute (Abs l r) x a = if or [x == l, member l (freeVars a)]
+substitute (Abs l r) x a = if or [x == l, S.member l (freeVars a)]
                            then Left l
                            else case substitute r x a of
                                  Left err    → Left err
                                  Right inner → Right $ Abs l inner
+
+
+-- De Bruijn
+data DBn = DVar Int | DApp DBn DBn | DAbs DBn
+instance Show DBn where
+  show (DVar i) = show i
+  show (DApp a b) = show a ++ " " ++ show b
+  show (DAbs c) = "λ(" ++ show c ++ ")"
+
+
+-- M (var → depth), current depth, Lambda
+lambdaToDBn' :: M.Map Literal Int → Int → Lambda → DBn
+lambdaToDBn' m d (Var t) = DVar $ if M.member t m
+                             then d - m M.! t
+                             else d + 1
+lambdaToDBn' m d (App a b) = DApp (lambdaToDBn' m d a) (lambdaToDBn' m d b)
+lambdaToDBn' m d (Abs l a) = DAbs $ lambdaToDBn' (M.insert l d m) (d + 1) a
+
+
+lambdaToDBn :: Lambda → DBn
+lambdaToDBn = lambdaToDBn' M.empty 1
 
 reduce :: Lambda → Lambda
 reduce o@(Var _) = o
@@ -46,23 +68,3 @@ reduce o@(App (Abs l x) t) = case substitute x l t of
   Right r → r
 reduce (App a b) = App (reduce a) (reduce b)
 reduce a = a
-
---substitute = substitute' empty
---
---substitute' :: Set Literal → Lambda → Literal → Lambda → Either Literal Lambda
---substitute' nonfree a@(Var l) var sub =
---  if not (var == l) then Right a else
---    case filter (\x → member x nonfree) (toList $ freeVars sub) of
---      []    → Right sub
---      (x:_) → Left x
---substitute' nonfree (App l r) var sub =
---  case ((substitute' nonfree l var sub), (substitute' nonfree r var sub)) of
---    (Right a, Right b) → Right $ App a b
---    (Left a, _)        → Left a
---    (_, b)             → b
---substitute' nonfree a@(Abs x l) var sub =
---  if x == var
---  then Right a
---  else case (substitute' (insert x nonfree) l var sub) of
---         Left y       → Left y
---         Right lambda → Right $ Abs x lambda
